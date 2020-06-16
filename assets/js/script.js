b5aab1f404f515a6d65065c3aa77a316e7522530
McClellan-Micah <mcclellan.micah.6@gmail.com>
2020-06-16 08:56:31 -0700

    use js to enable form-submit functionability

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,18 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+
+    //this will tell the browser not to automatically reload the page when the form is submitted
+    event.preventDefault();
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
 }
 
-buttonEl.addEventListener("click", createTaskHandler); {
+formEl.addEventListener("submit", createTaskHandler); {
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
